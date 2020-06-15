9bb37b2150b11bc427debc8876ace9c688ec5644
ktrnthsnr <asakthisner@hotmail.com>
2020-06-14 23:39:10 -0700

    updated connection.js to include env module

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
+const Sequelize = require('sequelize');
+
+// imported env module
+const env = require('../env');
+
 // dotenv 
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-
+// create connection to our mysql db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
       //  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    : new Sequelize('ecommerce_db', 'root', '..removed', 
+    : new Sequelize('ecommerce_db', 'root', env.password,
     {
       host: 'localhost',
       dialect: 'mysql',
@@ -16,4 +20,5 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
+//export
 module.exports = sequelize;
--- env.js
+++ env.js
@@ -1,11 +1,3 @@
-// connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: env.password,
-    database: 'ecommerce_db'
-  });
-
 module.exports = {
-    password : '',
+  password : 'UCBKata468)345'
 }
